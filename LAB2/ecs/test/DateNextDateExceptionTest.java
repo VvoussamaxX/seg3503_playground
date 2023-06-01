1b64b504331c57b6b8eaa113696eb107d0e06792
Mohamed oussama el malki <oelmalki2003@gmail.com>
2023-06-01 14:31:02 -0400

    last_changes

--- LAB2/ecs/test/DateNextDateExceptionTest.java
+++ LAB2/ecs/test/DateNextDateExceptionTest.java
@@ -1,14 +1,41 @@
+import static org.junit.Assert.assertEquals;
+
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.management.ConstructorParameters;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@RunWith(Parameterized.class)
 public class DateNextDateExceptionTest
 {
     private int year;
     private int month;
     private int day;
     private IllegalArgumentException expectedResult;
-
+    /**
+	 * Constructor for a parameterized test case.
+	 * </p>
+	 * <p>
+	 * The constructor stores the parameters in instance variables for use by
+	 * whichever test method is being run. Because we don't know exactly which
+	 * method will be run (in general), the parameters have to be set up for all
+	 * possible test methods for the set of values.
+	 *
+	 * @param year
+	 *            Bit value for left side of binary operator
+	 * @param month
+	 *            Bit value for right side of binary operator
+	 * @param day
+     * @param expectedResult
+	 *            Expected bit value for the result of AND
+	 */
     
 public DateNextDateExceptionTest(int year, int month, int day, IllegalArgumentException expectedResult){
 
@@ -17,7 +44,7 @@ public DateNextDateExceptionTest(int year, int month, int day, IllegalArgumentEx
     this.day = day;
     this.expectedResult=expectedResult; 
 }
-
+@Parameters
 public static List<Object[]> data( )
 {
     List<Object[]> params = new LinkedList<Object[]>( );
@@ -29,7 +56,10 @@ public static List<Object[]> data( )
     return params;
 }
 
-public void testDate() {
-    System.out.println(expectedResult);
-}
+@Test(expected = IllegalArgumentException.class)
+    public void testDate() {
+        Date date = new Date(year, month, day);
+        Date nextDate = date.nextDate();
+        assertEquals(expectedResult, nextDate);        
+    }
 }
--- LAB2/ecs/test/DateNextDateOkTest.java
+++ LAB2/ecs/test/DateNextDateOkTest.java
@@ -3,20 +3,41 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
+@RunWith(Parameterized.class)
 public class DateNextDateOkTest
 {
 
 private Date date;
 private Date expectedResult;
-
+/**
+	 * Constructor for a parameterized test case.
+	 * </p>
+	 * <p>
+	 * The constructor stores the parameters in instance variables for use by
+	 * whichever test method is being run. Because we don't know exactly which
+	 * method will be run (in general), the parameters have to be set up for all
+	 * possible test methods for the set of values.
+	 *
+	 * @param date
+	 *            Bit value for left side of binary operator
+	 *            Bit value for right side of binary operator
+	 * @param expectedResult
+	 *            Expected bit value for the result of AND
+	 */
 
 public DateNextDateOkTest(Date date, Date expectedResult) {
     this.date = date;
     this.expectedResult = expectedResult;
 }
 
+@Parameters
 public static List<Date[]> data( )
 {
     List<Date[]> params = new LinkedList<Date[]>( );
@@ -34,10 +55,11 @@ public static List<Date[]> data( )
     params.add(new Date[] {new Date(3456, 03,30), new Date(3456, 03,31)});
     params.add(new Date[] {new Date(1901, 07,31), new Date(1901, 8,01)});
     params.add(new Date[] {new Date(3453, 01,31), new Date(3453, 02,01)});
-    params.add(new Date[] {new Date(3456, 12,31), new Date(3456, 01,01)});
+    params.add(new Date[] {new Date(3456, 12,31), new Date(3457, 01,01)});
     return params;
 }
 
+@Test
 public void testDate(){
     Date actualDate = date.nextDate();
     assertEquals(expectedResult, actualDate);
